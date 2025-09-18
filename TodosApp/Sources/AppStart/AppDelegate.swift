8d5173240cd324164e463ff5c7e4a699c92c2ea6
ula0094 <ula0094@gmail.com>
2025-09-18 14:02:34 +0500

    let's statrt

---
+++ TodosApp/Sources/AppStart/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  TodosApp
+//
+//  Created by Ulugbek Mukhsinovich on 18/09/25.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let rootVC = MainModuleBuilder.build()
+        
+        let nav = UINavigationController(rootViewController: rootVC)
+        nav.navigationBar.prefersLargeTitles = true
+        window.rootViewController = nav
+        window.makeKeyAndVisible()
+    }
+}
---
+++ TodosApp/Tests/TodosAppTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class TodosAppTests: XCTestCase {
+    func test_twoPlusTwo_isFour() {
+        XCTAssertEqual(2+2, 4)
+    }
+}
